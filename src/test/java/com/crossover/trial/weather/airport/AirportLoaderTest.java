f0cc033bf4983a6ed53d548341ca64926f3ddd45
Ishim Mañón <Ishim Man>
2016-09-03 00:55:23 -0500

    Fix test

--- src/test/java/com/crossover/trial/weather/airport/AirportLoaderTest.java
+++ src/test/java/com/crossover/trial/weather/airport/AirportLoaderTest.java
@@ -1,5 +1,7 @@
 package com.crossover.trial.weather.airport;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -14,16 +16,20 @@ public class AirportLoaderTest {
 	 * @throws Exception
 	 */
 	@Test
-    public void testUpload() throws Exception {
+	public void testUpload() throws Exception {
 		String s = AirportLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath()+"airports.dat";
-        try {
-			AirportLoader.main(new String[] {s.substring(1)});
-		} catch (Exception e) {
-			e.printStackTrace();
+		try {
+			//Surefire does not support tests or any referenced libraries calling System.exit() at any time.
+			//According to http://maven.apache.org/surefire/maven-surefire-plugin/faq.html
+			//AirportLoader.main(new String[] {s.substring(1)});
+			AirportLoader al = new AirportLoader();
+			al.upload(new FileInputStream(s));
+		}catch(final FileNotFoundException ex){
+			ex.printStackTrace();
 		}
-        Assert.assertTrue("dat file uploaded OK with InputStream method", true);
-    }
-	
+		Assert.assertTrue("dat file uploaded OK with InputStream method", true);
+	}
+
 	/**
 	 * Test upload using scanner
 	 * @throws Exception
@@ -35,8 +41,10 @@ public class AirportLoaderTest {
 		final Scanner scanner;
 
 		try{
-			scanner = new Scanner(s);
+			File file = new File(s);
+			scanner = new Scanner(file);
 			al.upload(scanner);
+			scanner.close();
 		}catch(final FileNotFoundException ex){
 			ex.printStackTrace();
 		}
